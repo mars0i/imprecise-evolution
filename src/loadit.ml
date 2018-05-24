7c0c5739fe4dfa55f5709cd61997a680e0515042
mars0i <mabrams@uab.edu>
2018-05-23 21:16:48 -0500

    removed unused files from the impev repo

--- src/loadit.ml
+++ src/loadit.ml
@@ -1,10 +1,10 @@
 #require "parmap";;
 #load "utils.cma";;
 #load "models.cma";;
-module I = Models.CredalsetIO;;
-module S = Models.Setchains;;
-module T = Models.Tdists;;
+module I  = Models.CredalsetIO;;
+module S  = Models.Setchains;;
+module T  = Models.Tdists;;
 module TM = Models.Tranmats;;
-module W = Models.Wrightfisher;;
-module G = Utils.Genl;;
+module W  = Models.Wrightfisher;;
+module G  = Utils.Genl;;
 
