aa9fcb886c53e03e1f646e60d00716d46e1d944d
mars0i <mabrams@uab.edu>
2018-06-06 16:21:29 -0500

    renamed module alias

--- src/lib/models/app_fns.ml
+++ src/lib/models/app_fns.ml
@@ -4,7 +4,7 @@
  * Add option to control number of pmap forks.
  *)
 
-module S = Setchains
+module SC = Setchains
 module W = Wrightfisher
 module IO = CredalsetIO
 module T = Genstate
@@ -18,8 +18,8 @@ let commandline_make_setchain_and_save rows cols plot_max fontsize sample
                                            skip updown nofork basename popsize
 					   initfreq startgen lastgen fitn_floats () =
   let fitn_recs = W.group_fitns fitn_floats in  (* parse groups of three floats into separate fitness records *)
-  let selected_tdistlists = S.make_setchain_from_fitns ~verbose:true ~fork:(not nofork) ~skip 
-                             popsize initfreq startgen lastgen fitn_recs
+  let selected_tdistlists = SC.make_setchain_from_fitns ~verbose:true ~fork:(not nofork) ~skip 
+                              popsize initfreq startgen lastgen fitn_recs
   in
   Printf.printf "making pdfs ... \n%!";
   IO.make_setchain_bounds_pdfs ~colors:bottom_top_colors
