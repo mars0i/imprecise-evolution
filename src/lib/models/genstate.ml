2413e8cb906f9359a8a0e7a1e17d2e8242e70520
mars0i <mabrams@uab.edu>
2018-06-06 16:21:56 -0500

    added select_in_order: generalization of select
    And now I have a weird/subtle type error.

--- src/lib/models/genstate.ml
+++ src/lib/models/genstate.ml
@@ -1,6 +1,5 @@
 module Mat = Owl.Mat
-module S = Core.Sequence
-module Ss = Core.Sequence.Step
+module Sq = Core.Sequence
 
 let (%) f g = (fun x -> f (g x))
 
@@ -8,7 +7,7 @@ let always _ = true
 
 type genstate = {time : int ; state : Mat.mat list}
 
-type genstate_seq = genstate S.t
+type genstate_seq = genstate Sq.t
 
 (** genstate functions *) 
 
@@ -19,54 +18,64 @@ let make time state = {time ; state}
 
 (** genstate_seq functions *) 
 
-let hd = S.hd_exn
-let tl = S.tl_eagerly_exn
-let cons x xs = S.(append (singleton x) xs)
-let next = S.next
-let is_empty = S.is_empty
-let nth = S.nth_exn
-let fold_left f init seq = S.fold ~f ~init seq
-let map f xs = S.map ~f xs
-let iterate init f = S.memoize (S.unfold ~init ~f:(fun x -> Some (x, f x)))
-let take n seq = S.take seq n
-let take_while f seq = S.take_while ~f seq
-let drop n seq = S.drop seq n
-let drop_while f seq = S.drop_while ~f seq
-let to_list = S.to_list
+let hd = Sq.hd_exn
+let tl = Sq.tl_eagerly_exn
+let cons x xs = Sq.(append (singleton x) xs)
+let next = Sq.next
+let is_empty = Sq.is_empty
+let nth = Sq.nth_exn
+let fold_left f init seq = Sq.fold ~f ~init seq
+let map f xs = Sq.map ~f xs
+let iterate init f = Sq.memoize (Sq.unfold ~init ~f:(fun x -> Some (x, f x)))
+let take n seq = Sq.take seq n
+let take_while f seq = Sq.take_while ~f seq
+let drop n seq = Sq.drop seq n
+let drop_while f seq = Sq.drop_while ~f seq
+let to_list = Sq.to_list
 
 (* Note Core.Sequence.range doesn't allow an unbounded sequence. *)
 let ints ?(stride=1) init_n = iterate init_n (fun n -> n + stride)
 
-(* let map2 f xs ys = S.map ~f:(fun (x, y) -> f x y) (S.zip xs ys) *)
+(* let map2 f xs ys = Sq.map ~f:(fun (x, y) -> f x y) (Sq.zip xs ys) *)
 
 (* Based on Yaron Minsky's def at https://discuss.ocaml.org/t/how-to-write-map2-for-base-sequence/2090/3?u=mars0i *)
 let map2 f s1 s2 =
-  S.unfold ~init:(s1,s2)
+  Sq.unfold ~init:(s1,s2)
     ~f:(fun (s1,s2) ->
-      match S.next s1, S.next s2 with
+      match Sq.next s1, Sq.next s2 with
       | None, _ | _, None -> None
       | Some (x1, rest1), Some (x2, rest2) -> Some (f x1 x2, (rest1, rest2)))
 
 (**
 Example:
 {[
-    let vals = S.zip (S.repeat "foo") (S.range 0 10)
-    let keys = S.of_list [2;3;5]
-    select snd keys vals2 |> S.to_list
+    let vals = Sq.zip (Sq.repeat "foo") (Sq.range 0 10)
+    let keys = Sq.of_list [2;3;5]
+    select snd keys vals2 |> Sq.to_list
     - : (string * int) list = [("foo", 2); ("foo", 3); ("foo", 5)]
 ]}
 *)
 
-(* There's probably a more idiomatic way to do this. *)
-let select accessor keys vals =
-  S.unfold_step ~init:(keys, vals)
-                ~f:(fun (ks, vs) -> 
-		    if S.is_empty ks || S.is_empty vs then Ss.Done 
-		    else if hd ks = (accessor (hd vs))
-		    then Ss.Yield ((hd vs), (tl ks, tl vs))
-		    else Ss.Skip (ks, tl vs))
+let id x = x
 
-let subseq start finish ll = S.sub ll start (finish - start + 1)
+(* There's probably a more idiomatic way to do this. *)
+let select_in_order is_before accessor keys vals =
+  let open Core.Sequence in
+  let open Core.Sequence.Step in
+  unfold_step ~init:(keys, vals)
+    ~f:(fun (ks, vs) -> 
+         if is_empty ks || is_empty vs then Done 
+         else let k, v = hd_exn ks, hd_exn vs in
+              let vtag = accessor v in
+              if k = vtag
+	      then Yield (k,(tl_eagerly_exn ks, tl_eagerly_exn vs))
+              else if is_before k vtag   (* maybe there are gaps in vals *)
+              then Skip (ks, tl_eagerly_exn vs)  (* let vs catch up *)
+              else Skip (tl_eagerly_exn ks, vs)) (* let ks catch up *)
+
+let select = select_in_order (>)
+
+let subseq start finish ll = Sq.sub ll start (finish - start + 1)
 
 let ints_from n = iterate n ((+) 1) 
 
@@ -76,8 +85,8 @@ let add_times ?(first_tick=0) genstates =
 let remove_times genstates = map state genstates
 
 let sublist start_time finish_time genstates =
-  S.take_while ~f:(fun gs -> gs.time <= finish_time)
-                 (S.drop_while ~f:(fun gs -> gs.time < start_time)
+  Sq.take_while ~f:(fun gs -> gs.time <= finish_time)
+                 (Sq.drop_while ~f:(fun gs -> gs.time < start_time)
 		                 genstates)
 
 let select_by_times generations genstates =
--- src/lib/models/genstate.mli
+++ src/lib/models/genstate.mli
@@ -1,11 +1,11 @@
 module Mat = Owl.Mat
-module S = Core.Sequence
+module Sq = Core.Sequence
 
 (** Function that always returns [true] given any argument. *)
 val always : 'a -> bool
 
 type genstate = {time : int; state : Mat.mat list; }
-type genstate_seq = genstate S.t
+type genstate_seq = genstate Sq.t
 
 (** genstate functions *) 
 
@@ -15,72 +15,91 @@ val time : genstate -> int
 val state : genstate -> Mat.mat list
 val make : int -> Mat.mat list -> genstate
 
-val hd : 'a S.t -> 'a
-val tl : 'a S.t -> 'a S.t
-val next : 'a S.t -> ('a * 'a S.t) option
-val is_empty : 'a S.t -> bool
-val nth : 'a S.t -> int -> 'a
-val cons : 'a -> 'a S.t -> 'a S.t
-val fold_left : ('a -> 'b -> 'a) -> 'a -> 'b S.t -> 'a
-val map : ('a -> 'b) -> 'a S.t -> 'b S.t
-val map2 : ('a -> 'b -> 'c) -> 'a S.t -> 'b S.t -> 'c S.t
-val iterate : 'a -> ('a -> 'a) -> 'a S.t
-val take : int -> 'a S.t -> 'a S.t
-val take_while : ('a -> bool) -> 'a S.t -> 'a S.t
-val drop : int -> 'a S.t -> 'a S.t
-val drop_while : ('a -> bool) -> 'a S.t -> 'a S.t
-val to_list : 'a S.t -> 'a list
+val hd : 'a Sq.t -> 'a
+val tl : 'a Sq.t -> 'a Sq.t
+val next : 'a Sq.t -> ('a * 'a Sq.t) option
+val is_empty : 'a Sq.t -> bool
+val nth : 'a Sq.t -> int -> 'a
+val cons : 'a -> 'a Sq.t -> 'a Sq.t
+val fold_left : ('a -> 'b -> 'a) -> 'a -> 'b Sq.t -> 'a
+val map : ('a -> 'b) -> 'a Sq.t -> 'b Sq.t
+val map2 : ('a -> 'b -> 'c) -> 'a Sq.t -> 'b Sq.t -> 'c Sq.t
+val iterate : 'a -> ('a -> 'a) -> 'a Sq.t
+val take : int -> 'a Sq.t -> 'a Sq.t
+val take_while : ('a -> bool) -> 'a Sq.t -> 'a Sq.t
+val drop : int -> 'a Sq.t -> 'a Sq.t
+val drop_while : ('a -> bool) -> 'a Sq.t -> 'a Sq.t
+val to_list : 'a Sq.t -> 'a list
 
 
 (** [ints ~skip:n init_n] returns an infinite sequence of
     integers [~skip] apart starting from [init_n].  [skip]
     defaults to 1.  Giving it a negative value will produce a
     descending sequence. *)
-val ints : ?stride:int -> int -> int S.t
+val ints : ?stride:int -> int -> int Sq.t
+
+(** 
+In [select_in_order is_before accessor keys data], [keys] and [data] are lazy
+sequences.  The function returns a lazy sequence of elements from [data]
+such that [accessor val] is equal ([=]) to some element [keys].  Both 
+keys and the values by which elements of [data] are selected must be numbers
+and ordered so that they change monotonically according to boolean comparison 
+function [is_before]
+[>].  Example:
+{[
+    let vals = Sq.zip (Sq.repeat "foo") (Sq.range 0 10)
+    let keys = Sq.of_list [2;3;5]
+    select (>) snd keys vals2 |> Sq.to_list
+    - : (string * int) list = [("foo", 2); ("foo", 3); ("foo", 5)]
+]}
+*)
+val select_in_order : ('a -> 'a -> bool) -> ('b -> 'a) -> 'a Sq.t -> 'b Sq.t -> 'a Sq.t
 
 (** 
 In [select accessor keys data], [keys] and [data] are lazy
 sequences.  The function returns a lazy sequence of elements from [data]
-such that [accessor val] is equal to some element [keys].  Both 
-keys and the values by which elements of [data] are selected should
-have the same order in the sense that they both change monotonically
-in the same direction.  Example:
+such that [accessor val] is numerically equal to some element [keys].  Both 
+keys and the values by which elements of [data] are selected must be 
+monotonically increasing.  Example:
 {[
-    let vals = S.zip (S.repeat "foo") (S.range 0 10)
-    let keys = S.of_list [2;3;5]
-    select snd keys vals2 |> S.to_list
+    let vals = Sq.zip (Sq.repeat "foo") (Sq.range 0 10)
+    let keys = Sq.of_list [2;3;5]
+    select snd keys vals2 |> Sq.to_list
     - : (string * int) list = [("foo", 2); ("foo", 3); ("foo", 5)]
 ]}
 *)
-val select : ('a -> 'b) -> 'b S.t -> 'a S.t -> 'a S.t
+val select : ('a -> int) -> int Sq.t -> 'a Sq.t -> 'a Sq.t
+
+(** Identity function (useful with [select]). *)
+val id : 'a -> 'a
 
 (** Return a lazy list that's a sublist of the argument, from element start 
     (zero-based) to element finish, inclusive. *)
-val subseq : int -> int -> 'a S.t -> 'a S.t
+val subseq : int -> int -> 'a Sq.t -> 'a Sq.t
 
 (** [ints_from n] generates a lazy list of integers starting from [n]. *)
-val ints_from : int -> int S.t
+val ints_from : int -> int Sq.t
 
 (** [add_times genstate_seq] makes a lazy list of [genstate] from a lazy list
    of lists of state matrices, adding generation numbers in the [gen]
    field.  If [~first_tick] is given, start with that generation number;
    otherwise begin with generation 0. *)
-val add_times : ?first_tick:int -> Mat.mat list S.t -> genstate S.t
+val add_times : ?first_tick:int -> Mat.mat list Sq.t -> genstate Sq.t
 
 (** Reverse operation of [add_times]: Given a lazy list of [genstate],
     returns a lazy list of lists of state vectors. *)
-val remove_times : genstate S.t -> Mat.mat list S.t
+val remove_times : genstate Sq.t -> Mat.mat list Sq.t
 
 (** [sublist start_t finish_t genstate_seq] returns a lazy list that's
     a finite sublist of [genstate_seq], from the first element with 
     [gen] >= [start_time] to the last element with [gen] <= [finish_time].  
     Note that if the list is infinite and there are no elements satisfying
     both of these conditions, the function will try to run forever. *)
-val sublist : int -> int -> genstate S.t -> genstate S.t
+val sublist : int -> int -> genstate Sq.t -> genstate Sq.t
 
 (** In [select_by_times generations genstate_seq], [generations] is a lazy
     list of integers in increasing order, and [genstate_seq] is a lazy
     list of genstates.  The function returns a lazy list contanining those 
     genstate whose generation numbers match the integers in [generations]. *)
-val select_by_times : int S.t -> genstate S.t -> genstate S.t
+val select_by_times : int Sq.t -> genstate Sq.t -> genstate Sq.t
 
