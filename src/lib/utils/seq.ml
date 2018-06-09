a46802f3569dd283367a22fd348a46cbbfbf53c8
mars0i <mabrams@uab.edu>
2018-06-09 12:18:01 -0500

    made memoizing vs non-memoizing seq-generating fns

--- src/lib/utils/seq.ml
+++ src/lib/utils/seq.ml
@@ -4,6 +4,8 @@ module Step = S.Step
 
 type 'a t = 'a S.t
 
+let memoize = S.memoize
+
 let hd = S.hd_exn
 let tl = S.tl_eagerly_exn
 let cons x xs = S.(append (singleton x) xs) (* Inefficient(?) hack *)
@@ -11,30 +13,36 @@ let next = S.next
 let is_empty = S.is_empty
 let nth = S.nth_exn
 let fold_left f init seq = S.fold ~f ~init seq
-let map f xs = S.map ~f xs
-let iterate init f = S.memoize (S.unfold ~init ~f:(fun x -> Some (x, f x)))
+let map_raw f xs = S.map ~f xs
+let map f xs = memoize (map_raw f xs)
+let iterate_raw init f = S.unfold ~init ~f:(fun x -> Some (x, f x))
+let iterate init f = memoize (iterate_raw init f)
 let take n seq = S.take seq n
 let take_while f seq = S.take_while ~f seq
 let drop n seq = S.drop seq n
 let drop_while f seq = S.drop_while ~f seq
 let to_list = S.to_list
 
-let range ?(stride=1) first last =
+let range_raw ?(stride=1) first last =
   S.range ~start:`inclusive ~stop:`inclusive ~stride first last
 
-(* Note Core.Sequence.range doesn't allow an unbounded sequence. *)
-let ints ?(stride=1) init_n = iterate init_n (fun n -> n + stride)
+let range ?(stride=1) first last = memoize (range_raw ~stride first last)
 
+(* Note Core.Sequence.range doesn't allow an unbounded sequence. *)
+let ints_raw ?(stride=1) init_n = iterate init_n (fun n -> n + stride)
+let ints ?(stride=1) init_n = memoize (ints_raw ~stride init_n)
 
 let subseq start finish ll = S.sub ll ~pos:start ~len:(finish - start + 1)
 
 (* Based on Yaron Minsky's def at https://discuss.ocaml.org/t/how-to-write-map2-for-base-sequence/2090/3?u=mars0i *)
-let map2 f s1 s2 =
-  S.unfold ~init:(s1,s2)
-    ~f:(fun (s1,s2) ->
-      match S.next s1, S.next s2 with
+let map2_raw f xs1 xs2 =
+  S.unfold ~init:(xs1, xs2)
+    ~f:(fun (xs1, xs2) ->
+      match S.next xs1, S.next xs2 with
       | None, _ | _, None -> None
       | Some (x1, rest1), Some (x2, rest2) -> Some (f x1 x2, (rest1, rest2)))
+
+let map2 f xs1 xs2 = memoize (map2_raw f xs1 xs2)
 
 (* Alternative definition: *)
 (* let map2 f xs ys = S.map ~f:(fun (x, y) -> f x y) (S.zip xs ys) *)
--- src/lib/utils/seq.mli
+++ src/lib/utils/seq.mli
@@ -11,22 +11,64 @@ val is_empty : 'a S.t -> bool
 val nth : 'a S.t -> int -> 'a
 val cons : 'a -> 'a S.t -> 'a S.t
 val fold_left : ('a -> 'b -> 'a) -> 'a -> 'b S.t -> 'a
+
+val memoize : 'a S.t -> 'a S.t
+
+(** [map_raw f xs] maps function [f] over sequence [xs] without memoizing
+    the results of the mapping. That is, [f] will be reapplied every
+    time an element resulting sequence is subsequently evaluated. *)
+val map_raw : ('a -> 'b) -> 'a S.t -> 'b S.t
+
+(** [map f xs] maps function [f] over sequence [xs], memoizing the result 
+    of the mapping.  That is, [f] will not be reapplied to elements that
+    have previously been evaluated. *)
 val map : ('a -> 'b) -> 'a S.t -> 'b S.t
+
+(** [map2_raw f xs1 xs2] is like [map_raw], but [f] is applied to
+    corresponding pairs of elements of [xs1] and [xs2]. *)
+val map2_raw : ('a -> 'b -> 'c) -> 'a S.t -> 'b S.t -> 'c S.t
+
+(** [map2 f xs1 xs2] is like [map], but [f] is applied to
+    corresponding pairs of elements of [xs1] and [xs2]. *)
 val map2 : ('a -> 'b -> 'c) -> 'a S.t -> 'b S.t -> 'c S.t
+
+(** [iterate init f] generates a sequence [init, f init, f (f init), ...]
+    without memoizing the results of the applications. That is, elements
+    of sequence will be recomputed every time the are ievaluated. *)
+val iterate_raw : 'a -> ('a -> 'a) -> 'a S.t
+
+(** [iterate init f] generates a sequence [init, f init, f (f init), ...],
+    memoizing the results of the applications. That is, elements of the
+    sequence will be NOT be recomputed every time they are evaluated. *)
 val iterate : 'a -> ('a -> 'a) -> 'a S.t
+
 val take : int -> 'a S.t -> 'a S.t
 val take_while : ('a -> bool) -> 'a S.t -> 'a S.t
 val drop : int -> 'a S.t -> 'a S.t
 val drop_while : ('a -> bool) -> 'a S.t -> 'a S.t
 val to_list : 'a S.t -> 'a list
 
+(* [range_raw n m] returns a sequence of integers from [n] to [m]
+   inclusive, where each subsequent integer is [stride] plus the
+   previous one (default: 1).  Elements sequence will be recomputed 
+   every time they are evaluated. *)
+val range_raw : ?stride:int -> int -> int -> int S.t
+
+(* [range n m] returns a sequence of integers from [n] to [m]
+   inclusive, where each subsequent integer is [stride] plus the
+   previous one (default: 1).  Elements of the sequence will NOT 
+   be recomputed every time. *)
 val range : ?stride:int -> int -> int -> int S.t
 
 (* Note Core.Sequence.range doesn't allow an unbounded sequence. *)
-(** [ints ~skip:n init_n] returns an infinite sequence of
+(** [ints_raw ~skip:n init_n] returns an infinite sequence of
     integers [~skip] apart starting from [init_n].  [skip]
-    defaults to 1.  Giving it a negative value will produce a
-    descending sequence. *)
+    defaults to 1.  Giving [stride] a negative value will produce a
+    descending sequence.  Elements of the sequence will be recomputed 
+    every time they are evaluated. *)
+val ints_raw : ?stride:int -> int -> int S.t
+
+(** Like [ints], but existing results will not be recomputed. *)
 val ints : ?stride:int -> int -> int S.t
 
 (** Return a lazy list that's a sublist of the argument, from element start 
